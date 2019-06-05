51f1879f2a576f5c23a26f35efd8ef29c125bcb2
elisek-bc <elisekppns@gmail.com>
2019-06-05 11:40:16 +0200

    SELECT: Fix: 01 done

--- 01.RFW-SELECTS/02-Fix-It/oef01/js/script.js
+++ 01.RFW-SELECTS/02-Fix-It/oef01/js/script.js
@@ -1,2 +1,2 @@
-const writers = document.querySelectorAll(`episode-writer`);
-writers.forEach($writer => console.count($writer));
+const writers = document.querySelectorAll(`.episode-writer`);
+writers.forEach(($writer) => console.log($writer.textContent));
